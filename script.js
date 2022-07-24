4a9bcd83d4c8b1185998890c210dfce942d70cbe
Joallyson-Pereira <joallysonpere@gmail.com>
2022-07-24 19:48:00 -0300

    Novo

--- script.js
+++ script.js
@@ -6,22 +6,55 @@ const nameOfDespesa = document.querySelector('#NameOfDespesa')
 const botaoAdicionarNome = document.querySelector('#adicionarNomeDaDespesa')
 const inputNomeDespesa = document.querySelector('#nomeDespesa')
 const amountOfDespesa = document.querySelector('#amountOfDespesas')
+const inputQtd = document.querySelector('#qtd');
 const botaoAdicionarQuantidade = document.querySelector('#adicionarAmountOfDespesa')
 const amountAdded = document.querySelector('#quantidadeDeDespesaAdicionado')
 const botaoAdicionarvalor = document.querySelector('#adicionarWorthOfDespesa');
+const adicioneValor = document.querySelector('#worthOfDespesa')
 const inputValorDeDespesaAdded = document.querySelector('#valorDeDespesaAdicionado');
 const valor = document.querySelector('#valorDaDespesa');
+const lastQuestion = document.querySelector('#lastQuestion');
+const sim = document.querySelector('#yes');
+
+
+sim.addEventListener('click', function() {
+    removerShow(lastQuestion);
+    removerShow(amountOfDespesa)
+    removerShow(adicioneValor)
+    inputNomeDespesa.value = '';
+    removerShow(nameOfDespesa)
+
+    valor.value = 'R$ ';
+    removerShow(inputValorDeDespesaAdded);
+
+    inputQtd.value = '';
+    removerShow(amountAdded);
+
+})
 
 botaoAdicionar.addEventListener('click', function(){
+    if (inputSalario < 4 ) {
+        alert('Informe um valor') 
+        return
+    }
     adicionado()
 })
 
 
 botaoAdicionarQuantidade.addEventListener('click', function(){
+    if (inputQtd.value.length < 1) {
+        alert('Informe um valor');
+        return
+    }
     addClassShow(amountAdded)
+    addClassShow(adicioneValor)
 })
 
 botaoAdicionarNome.addEventListener('click', function(){
+    if (inputNomeDespesa.value.length < 1) {
+        alert('Informe algum nome');
+        return
+    }
     addClassShow(nameOfDespesa)
     addClassShow(amountOfDespesa)
 })
@@ -33,9 +66,7 @@ document.addEventListener("keypress", function(e) {
     }
 })*/
 
-botaoAdicionarQuantidade.addEventListener('click', function(){
-    addClassShow()
-})
+
 
 function adicionado() {
     if (inputSalario.value.length < 4) {
@@ -50,10 +81,16 @@ function addClassShow(e) {
     e.classList.add('show')
 }
 
+function removerShow(valor) {
+    valor.classList.remove('show');
+}
+
+
 botaoAdicionarvalor.addEventListener('click', function() {
     if (valor.value.length < 4) {
         alert('Informe algum valor')
         return
     };
     addClassShow(inputValorDeDespesaAdded)
+    addClassShow(lastQuestion)
 })
